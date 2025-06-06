00fc4a1701714ce8085ae709d12393343b4f6332
Artyom Opanchuk <subzero666@list.ru>
2025-06-06 21:09:38 +0300

    Добавил валидацию полей ФИО

--- compose/profiles/profiles/services/profiles/models.py
+++ compose/profiles/profiles/services/profiles/models.py
@@ -2,34 +2,42 @@ from __future__ import annotations
 
 import datetime
 import uuid
+from typing import Annotated
 
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    Field,
+)
 
 from ..common import PhoneNumberField
+
+LastNameField = Annotated[str, Field(min_length=1, max_length=256)]
+FirstNameField = Annotated[str, Field(min_length=1, max_length=256)]
+PatronymicField = Annotated[str, Field(max_length=256)]
 
 
 class ReadProfileResponse(BaseModel):
     id: uuid.UUID
     user_id: uuid.UUID
-    last_name: str
-    first_name: str
-    patronymic: str
+    last_name: LastNameField
+    first_name: FirstNameField
+    patronymic: PatronymicField
     phone_number: PhoneNumberField | None
     created: datetime.datetime
     modified: datetime.datetime
 
 
 class ProfileCreate(BaseModel):
-    last_name: str = ''
-    first_name: str = ''
-    patronymic: str = ''
+    last_name: LastNameField
+    first_name: FirstNameField
+    patronymic: PatronymicField
     phone_number: PhoneNumberField | None = None
 
 
 class ProfileUpdate(BaseModel):
-    last_name: str = ''
-    first_name: str = ''
-    patronymic: str = ''
+    last_name: LastNameField = ''
+    first_name: FirstNameField = ''
+    patronymic: PatronymicField = ''
     phone_number: PhoneNumberField | None = None
 
 
