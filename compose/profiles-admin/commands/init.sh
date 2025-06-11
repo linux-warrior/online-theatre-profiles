3daf38e4685c6eb28a29193929d4a69522f36076
Artyom Opanchuk <subzero666@list.ru>
2025-06-11 16:43:10 +0300

    Автоматическое создание суперпользователя

--- compose/profiles-admin/commands/init.sh
+++ compose/profiles-admin/commands/init.sh
@@ -3,3 +3,7 @@
 set -e
 
 python manage.py migrate --noinput
+
+export DJANGO_SUPERUSER_PASSWORD="$SUPERUSER_PASSWORD"
+
+python manage.py createsuperuser --noinput --username="$SUPERUSER_LOGIN" --email '' || :
