4757895e24805c5bc93bcb944cdb35ac1763a88a
Artyom Opanchuk <subzero666@list.ru>
2025-06-25 18:09:33 +0300

    Добавил возможность задать порядок сортировки

--- compose/profiles/profiles/services/pagination/models.py
+++ compose/profiles/profiles/services/pagination/models.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import datetime
+import enum
 import uuid
 from typing import Annotated
 
@@ -11,10 +12,16 @@ from pydantic import (
 )
 
 
+class SortOrder(enum.StrEnum):
+    ASC = 'asc'
+    DESC = 'desc'
+
+
 class PageParams(BaseModel):
     id: uuid.UUID | None = Field(default=None, alias='page_id')
     timestamp: datetime.datetime | None = Field(default=None, alias='page_timestamp')
     size: int | None = Field(default=None, ge=1, le=100, alias='page_size')
+    sort_order: SortOrder = SortOrder.ASC
 
 
 PageParamsDep = Annotated[PageParams, Depends()]
--- compose/profiles/profiles/services/pagination/paginator.py
+++ compose/profiles/profiles/services/pagination/paginator.py
@@ -14,7 +14,10 @@ from sqlalchemy import (
     ColumnElement,
 )
 
-from .models import PageParams
+from .models import (
+    PageParams,
+    SortOrder,
+)
 
 
 class AbstractPaginator[TSelectable: tuple[Any, ...]](abc.ABC):
@@ -42,27 +45,43 @@ class Paginator[TSelectable: tuple[Any, ...]](AbstractPaginator[TSelectable]):
 
     def get_page(self, *, page_params: PageParams) -> Select[TSelectable]:
         timestamp_clauses: list[ColumnElement[bool]] = []
+        is_descending = page_params.sort_order == SortOrder.DESC
 
         if page_params.timestamp is not None:
-            timestamp_clauses.append(
-                self.timestamp_column < page_params.timestamp,
-            )
+            timestamp_comparison_clause: ColumnElement[bool]
+
+            if is_descending:
+                timestamp_comparison_clause = self.timestamp_column < page_params.timestamp
+            else:
+                timestamp_comparison_clause = self.timestamp_column > page_params.timestamp
+
+            timestamp_clauses.append(timestamp_comparison_clause)
 
         if page_params.timestamp is not None and page_params.id is not None:
-            timestamp_clauses.append(
-                and_(
-                    self.timestamp_column == page_params.timestamp,
-                    self.id_column < page_params.id,
-                ),
-            )
+            id_comparison_clause: ColumnElement[bool]
 
-        page_size = page_params.size or self.page_size
+            if is_descending:
+                id_comparison_clause = self.id_column < page_params.id
+            else:
+                id_comparison_clause = self.id_column > page_params.id
+
+            timestamp_clauses.append(and_(
+                self.timestamp_column == page_params.timestamp,
+                id_comparison_clause,
+            ))
+
+        page_order_by_clauses: list[SQLColumnExpression[Any]] = [
+            self.timestamp_column,
+            self.id_column,
+        ]
+
+        if is_descending:
+            page_order_by_clauses = [order_by_clause.desc() for order_by_clause in page_order_by_clauses]
 
         return self.statement.where(
             or_(*timestamp_clauses) if timestamp_clauses else true(),
         ).limit(
-            page_size,
+            page_params.size or self.page_size,
         ).order_by(
-            self.timestamp_column.desc(),
-            self.id_column.desc(),
+            *page_order_by_clauses,
         )
